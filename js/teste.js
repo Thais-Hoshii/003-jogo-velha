a30169882428e84d0477fa5f5884d10f14c3218d
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-05-10 21:41:27 -0300

    Arquivo teste.js criado
    Criei um arquivo teste.js para testar a remoção da introdução do jogo. Como ainda não tenho certeza de como vou organizar os arquivos, deixei esse nome genérico.

---
+++ js/teste.js
@@ -0,0 +1,9 @@
+function query(txt) {return document.querySelector(txt);}
+
+let iniciar = query("#botao-inicio");
+iniciar.addEventListener("click", ()=> {
+    let intro = query("#intro");
+    intro.style.position = "absolute";
+    intro.style.top = "-9999px";
+    console.log("sucesso");
+});
